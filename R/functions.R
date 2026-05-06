eb47ce3b5d935b6c939940abab30d8f087ca56f2
Kristine Tursø Svinning Valeur <krisv@regsj.dk>
2026-05-06 10:42:55 +0200

    Added the function to read multiple files with shared filename. Added a global variable replacing the mother file_path.

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,5 @@
+# Functions ----
+
 read <- function(file_path, max_rows = 100) {
   data <- file_path %>%
     readr::read_csv(
@@ -6,4 +8,16 @@ read <- function(file_path, max_rows = 100) {
       n_max = max_rows,
     )
   return(data)
+}
+
+read_all <- function(filename, max_rows = 100) {
+  files <- .DATASET_DIR |>
+    fs::dir_ls(regexp = filename, recurse = TRUE)
+  data <- files |>
+    purrr::map(\(file) read(file, max_rows = max_rows)) |>
+    purrr::list_rbind(names_to = "file_path_id")
+  return(data)
+
+  # Global variables ----
+  .DATASET_DIR <- here::here("data-raw/nurses-stress/")
 }
